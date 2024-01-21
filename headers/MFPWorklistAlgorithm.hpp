ab879f13a33471294ad661b8ba12fac2553cac6d
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2024-01-20 22:43:29 -0800

    Little bit more progress

--- headers/MFPWorklistAlgorithm.hpp
+++ headers/MFPWorklistAlgorithm.hpp
@@ -1,13 +1,15 @@
 #include "datatypes.h"
+#include "abstract_store.hpp"
 
 class MFPWorklistAlgorithm {
 private:
 
     /*
      * This is our worklist. I made it a vector, but we can choose to use a
-     * stack or a queue here.
+     * stack or a queue here. Each string in the worklist must be a BasicBlock
+     * label.
      */
-    std::vector<BasicBlock> worklist;
+    std::vector<std::string> worklist;
 
     /*
      * The underlying program object.
@@ -15,11 +17,42 @@ private:
     Program p;
 
     /*
+     * Our bb2store is a map from a basic block name to an entry abstract store.
+     */
+    std::map<std::string, AbstractStore> bb2store;
+
+    /*
      * List of names of all the int-typed variables whose addresses were taken
      * using the $addrof command.
      */
     std::vector<std::string> addrof_ints;
     std::vector<std::string> global_ints;
+
+    /*
+     * Set all global variables and function parameters to TOP.
+     */
+    void init_entry_stores() {
+
+        for (const auto &func : p.funcs) {
+
+            /*
+             * For each basic block in the program, add it to bb2store.
+             */
+            for (const auto &basic_block : func.second.bbs) {
+
+                /*
+                 * Give each basic block an empty abstract store for now.
+                 */
+                bb2store[basic_block.second.label] = AbstractStore({});
+            }
+
+            /*
+             * Set each function parameter to TOP.
+             *
+             * TODO Is this only for "entry" or for all basic blocks?
+             */
+        }
+    }
 
     /*
      * Get the list of names of all the int-typed variables whose addresses were
@@ -65,7 +98,7 @@ public:
      *
      * TODO Add more constructors for ease of use.
      */
-    MFPWorklistAlgorithm(json j) : p(Program(j)) {
+    MFPWorklistAlgorithm(json j) : p(Program(j)),  {
     }
 
     /*
@@ -77,5 +110,22 @@ public:
          * First, let's compute our list of addrof_ints.
          */
         get_addrof_ints();
+
+        /*
+         * Initialize bb2store.
+         */
+        init_entry_stores();
+
+        /*
+         * The worklist starts off with only one basic block: "entry".
+         */
+        worklist.push_back("entry");
+
+        /*
+         * The core of the MFP worklist algorithm.
+         */
+        while (!worklist.empty()) {
+            std::string sigma = worklist.pop_back();
+        }
     }
 };

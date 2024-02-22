72659b9a04d2a167418d91a4fcf17bd9a3091b32
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2024-02-22 12:09:45 -0800

    Woo hoo got something working

--- constraint-generator/constraint-generator.cpp
+++ constraint-generator/constraint-generator.cpp
@@ -9,7 +9,7 @@
 /*
  * For debugging.
  */
-#define DEBUG(x) std::cout << "(" << __FILE_NAME__ << ":" << __LINE__ << ") " << x << std::endl;
+#define DEBUG(x) std::cout << "(" << __FILE_NAME__ << ":" << __LINE__ << ") " << x << std::endl
 
 /*
  * For globals and $alloc identifiers, func_name will be the empty string.
@@ -20,15 +20,21 @@ typedef struct SetVariable {
 } SetVariable;
 
 /*
- * TODO a constructor argument can be either a SetVariable or another
+ * We have to forward-declare a Constructor because a constructor argument can
+ * be either a SetVariable or another Constructor.
+ */
+typedef struct Constructor Constructor;
+
+typedef std::variant<SetVariable, Constructor> Term;
+
+/*
+ * TODO A constructor argument can be either a SetVariable or another
  * TODO Constructor.
  */
 typedef struct Constructor {
-    std::vector<SetVariable> args;
+    std::vector<Term> args;
     std::string name;
 } Constructor;
-
-typedef std::variant<SetVariable, Constructor> Term;
 
 /*
  * Define a projection of c^(-arg)(v).
@@ -52,7 +58,7 @@ struct Statement {
     Expression e2;
 };
 
-Statement get_copy_constraint(CopyInstruction copy) {
+Statement get_copy_constraint(CopyInstruction copy, std::string func_name) {
     DEBUG("Getting $copy constraint");
     SetVariable x;
     x.var_name = copy.lhs->name;
@@ -60,15 +66,19 @@ Statement get_copy_constraint(CopyInstruction copy) {
     /*
      * TODO Get function name somehow.
      */
-    x.func_name = "";
+    x.func_name = func_name;
     SetVariable y;
     y.var_name = copy.op->var->name;
-    y.func_name = "";
+    y.func_name = func_name;
     Statement s;
     s.e1 = y;
     s.e2 = x;
     return s;
 }
+
+/*
+ * TODO Add function name argument to all of these.
+ */
 
 Statement get_addrof_constraint(AddrofInstruction addrof) {
     DEBUG("Getting $addrof constraint");
@@ -101,10 +111,45 @@ Statement get_store_constraint(StoreInstruction store) {
 }
 
 /*
- * TODO
+ * TODO I should refactor this.
  */
 void print_constraint(Statement c) {
-    std::cout << "Yeah this is a statement all right" << std::endl;
+
+    /*
+     * First, let's look at e1.
+     */
+    std::string e1_str = "";
+    if (std::holds_alternative<Term>(c.e1)) {
+        Term e1_term = std::get<Term>(c.e1);
+        if (std::holds_alternative<SetVariable>(e1_term)) {
+            SetVariable e1_set_var = std::get<SetVariable>(e1_term);
+            e1_str = e1_set_var.func_name + "." + e1_set_var.var_name;
+        }
+    }
+
+    /*
+     * Now let's look at e2.
+     */
+    std::string e2_str = "";
+    if (std::holds_alternative<Term>(c.e2)) {
+        Term e2_term = std::get<Term>(c.e2);
+        if (std::holds_alternative<SetVariable>(e2_term)) {
+            SetVariable e2_set_var = std::get<SetVariable>(e2_term);
+            e2_str = e2_set_var.func_name + "." + e2_set_var.var_name;
+        }
+    }
+
+    /*
+     * Now let's actually print out our constraint.
+     */
+    std::cout << e1_str << " <= " << e2_str << std::endl;
+}
+
+/*
+ * Print a set variable without a trailing newline.
+ */
+void print_set_var(SetVariable s) {
+    std::cout << s.func_name << "." << s.var_name;
 }
 
 int main(int argc, char *argv[]) {
@@ -127,14 +172,14 @@ int main(int argc, char *argv[]) {
         for (const auto& instruction : bb.second->instructions) {
             switch (instruction->instrType) {
                 case CopyInstrType: {
-                    DEBUG("Saw a $copy")
+                    DEBUG("Saw a $copy");
                     CopyInstruction *copy = (CopyInstruction *) instruction;
 
                     /*
                      * If the LHS isn't a pointer, ignore it.
                      */
                     if (copy->lhs->type->indirection != 0) {
-                        constraints.push_back(get_copy_constraint(*copy));
+                        constraints.push_back(get_copy_constraint(*copy, p.funcs[argv[2]]->name));
                     }
                     break;
                 }
@@ -164,10 +209,17 @@ int main(int argc, char *argv[]) {
                     break;
                 }
                 default: {
-                    DEBUG("Unrecognized instruction type")
+                    DEBUG("Unrecognized instruction type");
                     break;
                 }
             } // End of switch-case
         }
+    }
+
+    /*
+     * Now print out all our constraints.
+     */
+    for (const auto &constraint : constraints) {
+        print_constraint(constraint);
     }
 }
